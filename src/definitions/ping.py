8fc4237e98f1d0597a090b001edbfc97f981f12f
Demeter <benmalmberg2@gmail.com>
2025-02-13 17:11:00 -0500

    starting over with go

--- src/definitions/ping.py
+++
@@ -1,7 +0,0 @@
-class ping:
-    def __init__(self):
-        self.ping = "ping"
-
-    @property
-    def ping(self):
-        return self._ping
--- src/run.py
+++
@@ -1,5 +0,0 @@
-from executor import execute_script
-
-if __name__ == "__main__":
-    command = "ls -la"
-    print(execute_script(command))
